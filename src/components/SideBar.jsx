f4f2339461c1890e689dde9ddef49e4e329c73b9
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-08-12 21:28:16 +0530

    navlink active

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom"
 
 export const SideBar = () => {
+  const getStyles = ({isActive}) => ({
+    color: isActive ? "white" : '#676767'
+  })
   return (
     <>
-      <NavLink to="/" className="text-white text-decoration-none d-block mb-4 mt-3">Dashboard</NavLink>
-      <NavLink to="/departments" className="text-white text-decoration-none d-block mb-4">Departments</NavLink>
-      <NavLink to="/products" className="text-white text-decoration-none d-block mb-4">Products</NavLink>
+      <NavLink style={getStyles} to="/" className=" text-decoration-none d-block mb-4 mt-3">Dashboard</NavLink>
+      <NavLink style={getStyles} to="/departments" className=" text-decoration-none d-block mb-4">Departments</NavLink>
+      <NavLink style={getStyles} to="/products" className=" text-decoration-none d-block mb-4">Products</NavLink>
     </>
   )
 }
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,13 +3,14 @@ import { InventoryContext } from "../../main"
 
 export const Dashboard = () => {
   const {state} = useContext(InventoryContext)
-  console.log(state)
+
   const data = state.inventory.reduce((acc, curr) => {
     acc['stockQuantity'] = acc['stockQuantity'] + curr.stock
     acc['delivered'] = acc['delivered'] + curr.delivered
     acc['lowStock'] = curr.stock <= 10 ? acc['lowStock']+=1 : acc['lowStock']
     return acc
   }, {stockQuantity: 0, delivered: 0, lowStock: 0})
+  
   return (
     <div className="row mt-3 ms-3">
       <div className="col-md-3 bg-secondary-subtle text-center p-2 rounded-2 m-1">
